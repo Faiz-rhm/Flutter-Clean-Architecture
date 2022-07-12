3fb0a296bf2063a843135d3850c52783dfadd9a3
Rahman <faiz199011@gmail.com>
2022-07-12 21:33:45 +0430

    Add text theme

---
+++ lib/theme/theme_shadows.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_biolerplate/theme/app_colors.dart';
+
+
+final List<Shadow> textDarkShadows = [
+  const Shadow(
+    color: brandColorThree,
+  ),
+];
+
+final List<Shadow> textLightShadows = [
+  const Shadow(
+    color: brandColorOne,
+  ),
+];
+
+final List<Shadow> iconDarkShadows = [
+  const Shadow(
+    color: brandColorThree,
+  ),
+];
+
+final List<Shadow> iconLightShadows = [
+  const Shadow(
+    color: brandColorOne,
+  ),
+];
