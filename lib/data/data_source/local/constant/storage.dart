deb40543ae74b541b1ade211a3d7acae853281f8
Rahman <faiz199011@gmail.com>
2022-07-15 16:31:08 +0430

    Add storage contants

---
+++ lib/data/data_source/local/constant/storage.dart
@@ -0,0 +1,7 @@
+class StorageConstant {
+  static const String user = 'user';
+  static const String accessToken = 'accessToken';
+  static const String refreshToken = 'refreshToken';
+  static const String isLoggedIn = 'isLoggedIn';
+  static const String theme = 'theme';
+}
