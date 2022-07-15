ba064ce13bcf1d5c024e6936ffd84875ef5cca97
Rahman <faiz199011@gmail.com>
2022-07-15 16:27:47 +0430

    Add checkbox theme

---
+++ lib/theme/check_box_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_biolerplate/theme/app_colors.dart';
+
+final CheckboxThemeData  appMaterialLightCheckBoxThemeData = CheckboxThemeData(
+  side: BorderSide(color: appLightColorScheme.surface),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5),
+  ),
+);
+
+final CheckboxThemeData appMaterialDarkCheckBoxThemeData = CheckboxThemeData(
+  side: BorderSide(color: appLightColorScheme.surface),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5),
+  ),
+);
