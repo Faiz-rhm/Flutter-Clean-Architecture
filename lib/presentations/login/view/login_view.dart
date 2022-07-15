09c1ffcfd3cd2ddf56e8445d2a547a22275b9c9b
Rahman <faiz199011@gmail.com>
2022-07-15 16:31:38 +0430

    Add login view

---
+++ lib/presentations/login/view/login_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_biolerplate/app/shared/sized_box.dart';
+import 'package:flutter_biolerplate/app/widgets/custom_app_bar.dart';
+import 'package:flutter_biolerplate/app/widgets/custom_text_field.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Login'),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            CustomTextField(
+              controller: TextEditingController(),
+              hint: 'Username',
+            ),
+            gapH16,
+            CustomTextField(
+              controller: TextEditingController(),
+              hint: 'password',
+            ),
+            gapH16,
+            ElevatedButton(
+              child: const Text('Login'),
+              onPressed: (){},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
