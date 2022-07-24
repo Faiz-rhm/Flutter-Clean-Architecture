7da589507e49a8e0de042f821b7c914a23187d45
Rahman <faiz199011@gmail.com>
2022-07-24 21:28:03 +0430

    Refactor app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,24 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+import 'package:flutter/material.dart';
+import 'package:flutter_biolerplate/app/l10n/l10n.dart';
+import 'package:flutter_biolerplate/presentations/login/view/login_view.dart';
+import 'package:flutter_biolerplate/theme/theme.dart';
 
-export 'view/app.dart';
+class App extends StatelessWidget {
+  const App();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      restorationScopeId: 'app',
+      theme: appLightThemeData,
+      darkTheme: appDarkThemeData,
+      themeMode: ThemeMode.light,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const LoginView(),
+    );
+  }
+}
