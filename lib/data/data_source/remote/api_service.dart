9108dd46c48c97e98d11048905c588af35410aad
Rahman <faiz199011@gmail.com>
2022-07-24 21:34:04 +0430

    Add api service

---
+++ lib/data/data_source/remote/api_service.dart
@@ -0,0 +1,214 @@
+import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:flutter_biolerplate/app/helper/typedefs.dart';
+import 'package:flutter_biolerplate/data/data_source/remote/api_interface.dart';
+import 'package:flutter_biolerplate/data/data_source/remote/custom_exceptions.dart';
+import 'package:flutter_biolerplate/data/data_source/remote/dio_service.dart';
+import 'package:flutter_biolerplate/data/data_source/remote/response_model.dart';
+
+/// A service class implementing methods for basic API requests.
+class ApiService implements ApiInterface {
+  ApiService(DioService dioService) : _dioService = dioService;
+  late final DioService _dioService;
+
+  /// An implementation of the base method for requesting collection of data
+  @override
+  Future<List<T>> getCollectionData<T>({
+    required String endpoint,
+    JSON? queryParams,
+    CancelToken? cancelToken,
+    CachePolicy? cachePolicy,
+    int? cacheAgeDays,
+    bool requiresAuthToken = true,
+    required T Function(JSON responseBody) converter,
+  }) async {
+    List<Object?> body;
+
+    try {
+      // Entire map of response
+      final data = await _dioService.get<List<Object?>>(
+        endpoint: endpoint,
+        cacheOptions: _dioService.globalCacheOptions?.copyWith(
+          policy: cachePolicy,
+          maxStale: cacheAgeDays != null
+              ? Nullable(Duration(days: cacheAgeDays))
+              : null,
+        ),
+        options: Options(
+          extra: <String, Object?>{
+            'requiresAuthToken': requiresAuthToken,
+          },
+        ),
+        queryParams: queryParams,
+        cancelToken: cancelToken,
+      );
+
+      // Items of table as json
+      body = data.body;
+    } on Exception catch (ex) {
+      throw CustomException.fromDioException(ex);
+    }
+
+    try {
+      // Returning the deserialized objects
+      return body.map((dataMap) => converter(dataMap! as JSON)).toList();
+    } on Exception catch (ex) {
+      throw CustomException.fromParsingException(ex);
+    }
+  }
+
+  /// An implementation of the base method for requesting a document of datas
+  @override
+  Future<T> getDocumentData<T>({
+    required String endpoint,
+    JSON? queryParams,
+    CancelToken? cancelToken,
+    CachePolicy? cachePolicy,
+    int? cacheAgeDays,
+    bool requiresAuthToken = true,
+    required T Function(JSON response) converter,
+  }) async {
+    JSON body;
+    try {
+      // Entire map of response
+      final data = await _dioService.get<JSON>(
+        endpoint: endpoint,
+        queryParams: queryParams,
+        cacheOptions: _dioService.globalCacheOptions?.copyWith(
+          policy: cachePolicy,
+          maxStale: cacheAgeDays != null
+              ? Nullable(Duration(days: cacheAgeDays))
+              : null,
+        ),
+        options: Options(
+          extra: <String, Object?>{
+            'requiresAuthToken': requiresAuthToken,
+          },
+        ),
+        cancelToken: cancelToken,
+      );
+
+      body = data.body;
+    } on Exception catch (ex) {
+      throw CustomException.fromDioException(ex);
+    }
+
+    try {
+      // Returning the deserialized object
+      return converter(body);
+    } on Exception catch (ex) {
+      throw CustomException.fromParsingException(ex);
+    }
+  }
+
+  /// An implementation of the base method for inserting [data] at
+  @override
+  Future<T> setData<T>({
+    required String endpoint,
+    required JSON data,
+    CancelToken? cancelToken,
+    bool requiresAuthToken = true,
+    required T Function(ResponseModel<JSON> response) converter,
+  }) async {
+    ResponseModel<JSON> response;
+
+    try {
+      // Entire map of response
+      response = await _dioService.post<JSON>(
+        endpoint: endpoint,
+        data: data,
+        options: Options(
+          extra: <String, Object?>{
+            'requiresAuthToken': requiresAuthToken,
+          },
+        ),
+        cancelToken: cancelToken,
+      );
+    } on Exception catch (ex) {
+      throw CustomException.fromDioException(ex);
+    }
+
+    try {
+      // Returning the serialized object
+      return converter(response);
+    } on Exception catch (ex) {
+      throw CustomException.fromParsingException(ex);
+    }
+  }
+
+  /// An implementation of the base method for updating [data]
+  @override
+  Future<T> updateData<T>({
+    required String endpoint,
+    required JSON data,
+    CancelToken? cancelToken,
+    bool requiresAuthToken = true,
+    required T Function(ResponseModel<JSON> response) converter,
+  }) async {
+    ResponseModel<JSON> response;
+
+    try {
+      // Entire map of response
+      response = await _dioService.patch<JSON>(
+        endpoint: endpoint,
+        data: data,
+        options: Options(
+          extra: <String, Object?>{
+            'requiresAuthToken': requiresAuthToken,
+          },
+        ),
+        cancelToken: cancelToken,
+      );
+    } on Exception catch (ex) {
+      throw CustomException.fromDioException(ex);
+    }
+
+    try {
+      // Returning the serialized object
+      return converter(response);
+    } on Exception catch (ex) {
+      throw CustomException.fromParsingException(ex);
+    }
+  }
+
+  /// An implementation of the base method for deleting [data]
+  @override
+  Future<T> deleteData<T>({
+    required String endpoint,
+    JSON? data,
+    CancelToken? cancelToken,
+    bool requiresAuthToken = true,
+    required T Function(ResponseModel<JSON> response) converter,
+  }) async {
+    ResponseModel<JSON> response;
+
+    try {
+      // Entire map of response
+      response = await _dioService.delete<JSON>(
+        endpoint: endpoint,
+        data: data,
+        options: Options(
+          extra: <String, Object?>{
+            'requiresAuthToken': requiresAuthToken,
+          },
+        ),
+        cancelToken: cancelToken,
+      );
+    } on Exception catch (ex) {
+      throw CustomException.fromDioException(ex);
+    }
+
+    try {
+      // Returning the serialized object
+      return converter(response);
+    } on Exception catch (ex) {
+      throw CustomException.fromParsingException(ex);
+    }
+  }
+
+  /// An implementation of the base method for cancelling
+  @override
+  void cancelRequests({CancelToken? cancelToken}) {
+    _dioService.cancelRequests(cancelToken: cancelToken);
+  }
+}
